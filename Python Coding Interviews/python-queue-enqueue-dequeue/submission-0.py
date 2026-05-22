04eeb0548a66e20edceba1ea8c4f38dd33bffd82
Sameer Jain <sameerjain501@gmail.com>
2026-05-22 00:36:23 -0400

    Add: python-queue-enqueue-dequeue - submission-0

---
+++ Python Coding Interviews/python-queue-enqueue-dequeue/submission-0.py
@@ -0,0 +1,20 @@
+from typing import List, Deque
+from collections import deque
+
+
+def rotate_list(arr: List[int], k: int) -> Deque[int]:
+    new_queue = Deque(arr)
+    while k > 0:
+        new_queue.append(new_queue.popleft())
+        k -=1
+    return new_queue
+        
+
+
+# do not modify below this line
+print(rotate_list([1, 2, 3, 4, 5], 0))
+print(rotate_list([1, 2, 3, 4, 5], 1))
+print(rotate_list([1, 2, 3, 4, 5], 2))
+print(rotate_list([1, 2, 3, 4, 5], 3))
+print(rotate_list([1, 2, 3, 4, 5], 4))
+print(rotate_list([1, 2, 3, 4, 5], 5))
